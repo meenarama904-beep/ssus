7d7701188f10719fbd0f5b22260ca031afddb78f
meenarama904-beep <meenarama904@gmail.com>
2026-07-16 17:12:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ st.set_page_config (
 page_title = "ssus",
 layout="wide"
 )
-st.title("smart student Utillity System")
+st.title(":rainbow[smart student Utillity System]")
 st.write("Student Mangement and utilities")
